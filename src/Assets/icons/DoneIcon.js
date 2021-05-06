9c25859ab41cf591c7db5511d4ba2919f0306e62
MochIsrin068 <isrin068@gmail.com>
2021-05-06 13:14:00 +0700

    add icon and image assets

---
+++ src/Assets/icons/DoneIcon.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function DoneIcon(props) {
+  return (
+    <svg
+      width={14}
+      height={14}
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M7 0a7 7 0 10.001 14.001A7 7 0 007 0zm3.023 4.714l-3.29 4.563a.497.497 0 01-.808 0l-1.948-2.7a.125.125 0 01.101-.199h.733c.16 0 .31.077.405.208L6.328 8.13l2.456-3.407a.5.5 0 01.405-.207h.733c.101 0 .16.115.101.198z"
+        fill="#52C41A"
+      />
+    </svg>
+  );
+}
+
+export default DoneIcon;
---
+++ src/Assets/icons/MoreIcon.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function MoreIcon(props) {
+  return (
+    <svg
+      width={16}
+      height={4}
+      viewBox="0 0 16 4"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M.125 1.977a1.313 1.313 0 102.625 0 1.313 1.313 0 00-2.625 0zm6.563 0a1.313 1.313 0 102.625 0 1.313 1.313 0 00-2.626 0zm6.562 0a1.313 1.313 0 102.625 0 1.313 1.313 0 00-2.625 0z"
+        fill="#828282"
+      />
+    </svg>
+  );
+}
+
+export default MoreIcon;
